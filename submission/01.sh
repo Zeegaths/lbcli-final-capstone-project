53e23e98cff50156e4cd5d24507c968a9e2edc8a
Zeegaths <your.email@example.com>
2025-04-24 14:30:43 +0300

    solution

--- submission/01.sh
+++ submission/01.sh
@@ -2,13 +2,4 @@
 
 # What is the hash of block 243,833?
 
-BLOCK_HASH=$(bitcoin-cli -signet getblockhash 243833)
-echo "Block hash for 243833: $BLOCK_HASH"
-
-# Get detailed block information
-BLOCK_DATA=$(bitcoin-cli -signet getblock $BLOCK_HASH 2)
-
-# Count inputs across all transactions
-# We'll parse this using jq to count all inputs in all transactions
-INPUT_COUNT=$(echo "$BLOCK_DATA" | jq '[.tx[] | .vin | length] | add')
-echo "Total input count: $INPUT_COUNT"
+bitcoin-cli -signet getblockhash 243833
