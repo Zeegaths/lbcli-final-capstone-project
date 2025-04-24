2010aa002243d366b72f27e6d5263d1c2c81d5c0
Zeegaths <your.email@example.com>
2025-04-24 18:33:35 +0300

    solution

--- submission/07.sh
+++ submission/07.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # What is the coinbase tx in block 243,834?
@@ -6,9 +5,5 @@
 # Get block hash
 BLOCKHASH=$(bitcoin-cli -signet getblockhash 243834)
 
-# Get the raw hex of the coinbase transaction by:
-# 1. Getting the first transaction ID
-TXID=$(bitcoin-cli -signet getblock $BLOCKHASH | jq -r '.tx[0]')
-
-# 2. Getting the raw transaction hex
-bitcoin-cli -signet getrawtransaction $TXID
+# Get the first transaction ID (coinbase)
+bitcoin-cli -signet getblock $BLOCKHASH | jq -r '.tx[0]'
