9117effeb19763af088cc0f9d4b6d57eaf78aa80
Zeegaths <your.email@example.com>
2025-04-24 14:57:22 +0300

    solution

--- submission/08.sh
+++ submission/08.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# What block height was this tx mined?
-# 49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb
+# What is the coinbase tx in this block 243,834
 
-# Get transaction data
-TX_DATA=$(bitcoin-cli -signet getrawtransaction 49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb 2)
+# Get block hash
+BLOCK_HASH=$(bitcoin-cli -signet getblockhash 243834)
 
-# Extract the block hash and then get the block height
-BLOCK_HASH=$(echo "$TX_DATA" | jq -r '.blockhash')
-bitcoin-cli -signet getblock $BLOCK_HASH | jq -r '.height'
+# Get the coinbase transaction ID (first transaction in the block)
+bitcoin-cli -signet getblock $BLOCK_HASH 1 | jq -r '.tx[0]'
