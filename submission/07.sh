2b0c0af643bc3bd011caa7421275235bdc0c7629
Zeegaths <your.email@example.com>
2025-04-24 18:09:31 +0300

    solution

--- submission/07.sh
+++ submission/07.sh
@@ -1,7 +1,4 @@
-# what is the coinbase tx in this block 243,834
-# First get the block hash
-blockhash=$(bitcoin-cli -signet getblockhash 243834)
-echo "Block hash: $blockhash"
+#!/bin/bash
 
-# Then get the block with that hash
-bitcoin-cli -signet getblock "$blockhash" | jq -r '.tx[0]'
+# What is the coinbase tx in block 243,834?
+bitcoin-cli -signet getblockhash 243834 | xargs bitcoin-cli -signet getblock 2 | jq -r '.tx[0].txid'
--- submission/08.sh
+++ submission/08.sh
@@ -1,2 +1,13 @@
-# what block height was this tx mined ?
+#!/bin/bash
+
+# What block height was this tx mined?
 # 49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb
+
+# Get transaction data with full information
+TX_DATA=$(bitcoin-cli -signet getrawtransaction 49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb 2)
+
+# Extract the block hash
+BLOCK_HASH=$(echo "$TX_DATA" | jq -r '.blockhash')
+
+# Get the block height
+bitcoin-cli -signet getblock $BLOCK_HASH | jq -r '.height'
