d84d0d8547deda5fb89682dc8f67e9a4dd28d2da
Zeegaths <your.email@example.com>
2025-04-24 14:49:56 +0300

    solution

--- submission/06.sh
+++ submission/06.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
 # Only one tx in block 243,821 signals opt-in RBF. What is its txid?
+
+# Get block hash
+BLOCK_HASH=$(bitcoin-cli -signet getblockhash 243821)
+
+# Get all transactions in the block
+bitcoin-cli -signet getblock $BLOCK_HASH 2 | jq -r '.tx[] | select(.vin[] | .sequence < 0xffffffff - 1) | .txid' | head -n 1
