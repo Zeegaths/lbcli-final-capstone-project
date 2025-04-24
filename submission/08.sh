e7f220a53775b9fa02e9e11a2eb57194da523df4
Zeegaths <your.email@example.com>
2025-04-24 15:04:33 +0300

    solution

--- submission/08.sh
+++ submission/08.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # What is the coinbase tx in block 243,834?
-bitcoin-cli -signet getblockhash 243834 | xargs bitcoin-cli -signet getblock | jq -r '.tx[0]'
+bitcoin-cli -signet getblockhash 243834 | xargs bitcoin-cli -signet getblock 2 | jq -r '.tx[0].txid'
