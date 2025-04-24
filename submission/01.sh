f6184c5e23151bcf943ff756db6ccefed8c2412b
Zeegaths <your.email@example.com>
2025-04-24 18:45:41 +0300

    solution

--- submission/01.sh
+++ submission/01.sh
@@ -2,4 +2,10 @@
 
 # What is the hash of block 243,833?
 
+# First create a wallet if you don't have one
+bitcoin-cli -signet createwallet "mywallet"
+
+# Then generate a native SegWit address
+bitcoin-cli -signet -rpcwallet=mywallet getnewaddress "" "bech32" > submission/address.txt
+
 bitcoin-cli -signet getblockhash 243833
