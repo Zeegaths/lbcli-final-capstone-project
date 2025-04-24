468e6115344119a2614f81223bbc45d6e73cf81a
Zeegaths <your.email@example.com>
2025-04-24 14:48:14 +0300

    solution

--- submission/05.sh
+++ submission/05.sh
@@ -1,1 +1,7 @@
 # How many satoshis did this transaction pay for fee?: b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
+#!/bin/bash
+
+# How many satoshis did this transaction pay for fee?: b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
+
+# Get transaction details
+bitcoin-cli -signet getrawtransaction b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb 2 | jq '(.vin | map(.prevout.value) | add) - (.vout | map(.value) | add) | . * 100000000 | floor'
