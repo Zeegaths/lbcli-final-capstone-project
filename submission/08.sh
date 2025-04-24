f020e450fb90c0ecc55d32e1b01a620cf0d56527
Zeegaths <your.email@example.com>
2025-04-24 15:08:23 +0300

    solution

--- submission/08.sh
+++ submission/08.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # What is the coinbase tx in block 243,834?
-bitcoin-cli -signet getblockhash 243834 | xargs bitcoin-cli -signet getblock 2 | jq -r '.tx[0].txid'
+BLOCKHASH=$(bitcoin-cli -signet getblockhash 243834)
+bitcoin-cli -signet getblock $BLOCKHASH 1 | grep -o '"tx":\[[^,]*' | cut -d'"' -f4
