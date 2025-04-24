b21426d84a4a40f8feac74b8a5f3282217839588
Zeegaths <your.email@example.com>
2025-04-24 14:59:26 +0300

    solution

--- submission/08.sh
+++ submission/08.sh
@@ -2,8 +2,8 @@
 
 # What is the coinbase tx in this block 243,834
 
-# Get block hash
+# Get the hash of block 243,834
 BLOCK_HASH=$(bitcoin-cli -signet getblockhash 243834)
 
 # Get the coinbase transaction ID (first transaction in the block)
-bitcoin-cli -signet getblock $BLOCK_HASH 1 | jq -r '.tx[0]'
+bitcoin-cli -signet getblock $BLOCK_HASH | jq -r '.tx[0]'
