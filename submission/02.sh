b68507ff88fab543b2c7f70250c48b0c63610ae8
Zeegaths <your.email@example.com>
2025-04-24 14:31:42 +0300

    solution

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
 # How many new outputs were created by block 243,825?
+
+# Get the block hash
+BLOCK_HASH=$(bitcoin-cli -signet getblockhash 243825)
+
+# Get block details with full transaction information
+BLOCK_DATA=$(bitcoin-cli -signet getblock $BLOCK_HASH 2)
+
+# Count the outputs across all transactions in the block
+OUTPUT_COUNT=$(echo "$BLOCK_DATA" | jq '[.tx[] | .vout | length] | add')
+
+# Print just the number
+echo $OUTPUT_COUNT
